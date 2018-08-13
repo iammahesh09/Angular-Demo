0df4083e5a6199a8fd05b0e9aa1fe4c32fc8ee1b
iammahesh09 <iammahesh.nlr@gmail.com>
2018-08-13 17:59:12 +0530

    Welcome to Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,20 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './appRouting'
+
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MainMenuComponent } from './main/main.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './login/login.component';
-import { ProductDetailsComponent } from './products/product-list/product-details/product-details.component';
-import { UsersComponent } from './users/users.component';
-import { UserDetailsComponent } from './users/user-details/user-details.component';
-import { MobileComponent } from './mobile/mobile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    MainMenuComponent,
-    ProductListComponent,
-    ContactComponent,
-    LoginComponent,
-    ProductDetailsComponent,
-    UsersComponent,
-    UserDetailsComponent,
-    MobileComponent
+  AppComponent,
   ],
   imports: [
-    BrowserModule,
-    RouterModule,
-    FormsModule,
-    HttpClientModule,
-    AppRoutingModule
+  BrowserModule,
+  RouterModule,
+  FormsModule,
+  HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
